55b78005e09ee9a25a636c1254d62c24a01a0260
pmoulon <pmoulon@6aa038ce-0b7a-11df-90cd-0fbaec372790>
2010-03-09 07:19:13 +0000

    Fix random sampler implementation. Fix extractor implementation (Will be deleted later). WIP ( the code is exactly a copy of matlab for the moment, but we are close).

--- trunk/cpp/tests/mainTestRansac_LineFitting.cpp
+++ trunk/cpp/tests/mainTestRansac_LineFitting.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
 
 #include "ransac/ransac.h"
@@ -12,24 +13,46 @@ using namespace groupsac;
 // | :: estimators
 // | :: ransac
 
+#include "armadillo/include/armadillo"
+using namespace arma;
+// |
+// | :: mat
+// | :: vec
+
 //-- Temporary function (Must be defined more generally).
 //---- Deterministic sampler
 //---- Random sampler
 template <typename T>
 vector<int> sampler(const T & candidates, int MININUM_SAMPLES)
 {
-  return vector<int>();
+  /*vector<int> array;
+  array.push_back(0);
+  array.push_back(1);
+  return array;*/
+
+  vector<int> array;
+  int i = 0;
+  std::set<int> sample_set;
+  while (i < MININUM_SAMPLES)
+  {
+    int random_value_in_range = rand() % candidates.size();
+    if (sample_set.insert(random_value_in_range).second)
+    {
+      array.push_back(random_value_in_range);
+      ++i;
+    }
+  }
+  return array;
 }
-class mat
-{
-};
-class vec
-{
-};
+
 template<typename T>
 T extractor(const T & data, const vector<int> & sampled)
 {
-  return data;
+  mat test;
+  test.zeros(sampled.size(), data.n_cols);
+  for(size_t i=0; i < sampled.size(); ++i)
+    test.row(i) = data.row( sampled[i] );
+  return test;
 }
 
 //-- END -- Must now be defined
@@ -42,8 +65,12 @@ int main()
   estimators::Solver<mat,vec> * ptrSolver = new estimators::lineFittingSolver<mat,vec>;
 
   //-- Create input data
-  mat dataPoints;
-  // Todo(pmoulon) Initialize dataPoints
+  //mat dataPoints = "1 2; 2 4; 2 5; 3 6; 4 7";//"1 6; 2 5; 3 7; 4 10";
+  mat dataPoints = "0 0; 1 1; 2 2; 3 3";
+  /*vector<vec> models;
+  ptrSolver->solve(dataPoints, models);
+  ((estimators::lineFittingSolver<mat,vec>*)ptrSolver)->defaultEvaluator(models, dataPoints, 2.0);
+  */
 
   ransac::Ransac_RobustEstimator
   (
