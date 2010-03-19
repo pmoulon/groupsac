f8993c4c985c0742b8a13c56e1372d147091298b
pmoulon <pmoulon@6aa038ce-0b7a-11df-90cd-0fbaec372790>
2010-03-19 06:32:04 +0000

    Initial revision of 0.9.4 armadillo library. DO not compile yet for groupsac purpuse.

---
+++ cpp/thirdParty/armadillo/include/armadillo_bits/config.hpp.cmake
@@ -0,0 +1,39 @@
+// Copyright (C) 2010 NICTA and the authors listed below
+// http://nicta.com.au
+// 
+// Authors:
+// - Conrad Sanderson (conradsand at ieee dot org)
+// 
+// This file is part of the Armadillo C++ library.
+// It is provided without any warranty of fitness
+// for any purpose. You can redistribute this file
+// and/or modify it under the terms of the GNU
+// Lesser General Public License (LGPL) as published
+// by the Free Software Foundation, either version 3
+// of the License or (at your option) any later version.
+// (see http://www.opensource.org/licenses for more info)
+
+
+
+#cmakedefine ARMA_HAVE_STD_ISFINITE
+#cmakedefine ARMA_HAVE_STD_ISINF
+#cmakedefine ARMA_HAVE_STD_ISNAN
+#cmakedefine ARMA_HAVE_STD_SNPRINTF
+
+#cmakedefine ARMA_HAVE_LOG1P
+#cmakedefine ARMA_HAVE_GETTIMEOFDAY
+
+#cmakedefine ARMA_USE_ATLAS
+#cmakedefine ARMA_USE_LAPACK
+#cmakedefine ARMA_USE_BLAS
+#cmakedefine ARMA_USE_BOOST
+#cmakedefine ARMA_USE_BOOST_DATE
+
+#cmakedefine ARMA_EXTRA_DEBUG
+#cmakedefine ARMA_NO_DEBUG
+
+#if defined(ARMA_USE_ATLAS)
+  #if !defined(ARMA_ATLAS_INCLUDE_DIR)
+    #define ARMA_ATLAS_INCLUDE_DIR  ${ARMA_ATLAS_INCLUDE_DIR}
+  #endif
+#endif
