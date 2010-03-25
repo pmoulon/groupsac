7ec9e08f601339d509bc5b60c705e8fe252e588f
nikai97 <nikai97@6aa038ce-0b7a-11df-90cd-0fbaec372790>
2010-03-25 08:06:29 +0000

    added CppUnitLite

---
+++ cpp/tests/testLineFittingSolver.cpp
@@ -0,0 +1,25 @@
+/*
+ * testLineFitting.cpp
+ *
+ *   Created on: Mar 25, 2010
+ *       Author: nikai
+ *  Description: unit tests for line fitting
+ */
+
+#include "CppUnitLite/TestHarness.h"
+
+TEST ( LineFittingSolver, demo )
+{
+    LONGS_EQUAL(1, 1);
+    CHECK(true);
+}
+
+TEST ( LineFittingSolver, solve )
+{
+    // some test function for solve
+}
+
+/* ************************************************************************* */
+int main() { TestResult tr; return TestRegistry::runAllTests(tr);}
+/* ************************************************************************* */
+
