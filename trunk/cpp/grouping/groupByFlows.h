cde0d4a5abb39cd4305581cec7b65fa225646366
pmoulon <pmoulon@6aa038ce-0b7a-11df-90cd-0fbaec372790>
2010-06-23 14:37:34 +0000

    Add Grouping directory. WIP (Optical Flow point based segmentation).

---
+++ trunk/cpp/grouping/groupByFlows.h
@@ -0,0 +1,73 @@
+
+
+#include <iostream>
+
+#include "MeanShift/MeanShiftCluster.h"
+#include "armadillo/include/armadillo"
+using namespace arma;
+// |
+// | :: mat
+// | :: vec
+
+namespace groupsac  {
+namespace grouping  {
+
+/// use Optical Flow based segmentation to group the data points for GroupSAC
+bool groupByFlows(const mat & xs1, const mat & xs2, double bandwidth, int & seg_num, mat & vis_map, mat & clustCent, bool bVerbose = false)
+{
+  using namespace std;
+  //% output:
+  //% seg_num -   the total number of segments
+  //% vis_map -   the row is the point index, the column is the segment index
+  //% clustCent - the cluster centers
+
+
+  //%% generate the flow groups
+  mat x_dists = xs1.row(0) - xs2.row(0);
+  mat y_dists = xs1.row(1) - xs2.row(1);
+  mat xs = xs1;
+  xs = join_cols(xs, x_dists);
+  xs = join_cols(xs, y_dists);
+
+  mat cluster2dataCell;
+  vector<int> data2cluster;
+  MeanShiftCluster(xs, bandwidth, clustCent, data2cluster, cluster2dataCell);
+  
+  // TODO CUSTOMIZE THE SQ_DIST
+  
+  if (bVerbose)
+  {
+      cout << "clustering takes X seconds" << endl;
+  }
+  seg_num = clustCent.n_cols;
+
+  // TODO implement the following :
+  //-----------------------------
+/*  //%% reorder the groups according to the number of the points they contain
+  group_sizes = zeros(seg_num ,1);
+  for (int gi=1; <i<length(group_sizes); ++gi)
+  {
+      group_sizes(gi) = length(clustMembsCell{gi});
+  }
+  [sorted idx] = sort(group_sizes, 'descend');
+
+  //%% generate vis_map using the reordered 
+  datum_num = size(xs1, 2);
+  vis_map = zeros(datum_num, seg_num);
+  for (int gi = 1; gi < seg_num ; ++gi)
+  {
+      vis_map(clustMembsCell{gi},idx==gi) = 1;
+  }*/
+
+/*      function [dists] = sqdist(x1s, x2)
+          x1_num = size(x1s,2);
+          weights = [1 1 10 10];
+          dists = weights * ((repmat(x2,1,x1_num) - x1s).^2);    %dist squared from mean to all points still active
+      end
+  end*/
+  //-----------------------------
+  
+  return true; //TODO make a better return
+}
+}; // namespace grouping
+}; // namespace groupsac
---
+++ trunk/cpp/grouping/testGroupByFlows.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <memory>
+using namespace std;
+
+#include "CppUnitLite/TestHarness.h"
+#include "grouping/groupByFlows.h"
+using namespace groupsac::grouping;
+
+//using namespace groupsac;
+// |
+// | :: estimators
+// | :: ransac
+// | :: grouping /!\ TODO
+
+#include "armadillo/include/armadillo"
+using namespace arma;
+// |
+// | :: mat
+// | :: vec
+
+//------------------------------
+// Assert that test works
+//------------------------------
+TEST ( Group_by_Flows, demo )
+{
+    LONGS_EQUAL(1, 1);
+    CHECK(true);
+}
+
+//------------------------------
+// Test : Ask one cluster
+// - given a point cloud
+// - ask for one cluster
+// - assert that all point are assigned to the same cluster
+// - check geometric center of the cluster
+//------------------------------
+TEST ( Group_by_Flows, TestOneCluster )
+{
+  mat xs1 = "1 2 3 4 5; 0 0 0 0 0";
+  mat xs2 = "2 3 4 5 6; 0 0 0 0 0";
+  double bandwidth = 10.0;
+  int seg_num;
+  mat vis_map, clustCent;
+  CHECK( true == 
+          groupByFlows(xs1, xs2, bandwidth, seg_num, vis_map, clustCent));
+
+  // Check return values :
+  CHECK( 1 == seg_num);
+
+  mat expected_vis_map = "1; 1; 1; 1; 1";
+  //TODO => CHECK( accu(vis_map == expected_vis_map) == expected_vis_map.n_elem );
+  
+  mat expected_clustCent = "3; 0; -1; 0"; // x, y, delta_x, delta_y
+  CHECK( accu(clustCent == expected_clustCent) == expected_clustCent.n_elem );
+}
+
+//------------------------------
+// Test : Ask two cluster
+// - given a point cloud
+// - ask for two cluster
+// - assert that all points are assigned to the good cluster
+// - check geometric center of the clusters
+//------------------------------
+TEST ( Group_by_Flows, TestTwoCluster )
+{
+  mat xs1 = " 1  2  3  4  5  6  7  8  9 10;0 0 0 0 0 0 0 0 0 0";
+  mat xs2 = "11 12 13 14 15 -4 -3 -2 -1  0;0 0 0 0 0 0 0 0 0 0";
+  double bandwidth = 10.0;
+  int seg_num;
+  mat vis_map, clustCent;
+  CHECK( true == 
+          groupByFlows(xs1, xs2, bandwidth, seg_num, vis_map, clustCent));
+
+  // Check return values :
+  CHECK( 2 == seg_num);
+
+  mat expected_vis_map = "1 1 1 1 1 0 0 0 0 0; 0 0 0 0 0 1 1 1 1 1";
+  vis_map = trans(vis_map);
+  //TODO => CHECK( accu(vis_map == expected_vis_map) == expected_vis_map.n_elem );
+  
+//  mat expected_clustCent = "3 0 -10 0; 8 0 10 0"; // x, y, delta_x, delta_y
+  mat expected_clustCent = "8 0 10 0; 3 0 -10 0"; // x, y, delta_x, delta_y
+  expected_clustCent = trans(expected_clustCent);
+  CHECK( accu(clustCent == expected_clustCent) == expected_clustCent.n_elem );
+}
+
+//------------------------------
+// Test : Ask two cluster
+// - given a point cloud
+// - ask for two cluster
+// - assert that all points are assigned to the good cluster
+// - check geometric center of the c
+//------------------------------
+TEST ( Group_by_Flows, TestTwoCluster2 )
+{
+  mat xs1 = "1 2 3 4 5   6   7    8    9  10; 0 0 0 0 0 0 0 0 0 0";
+  mat xs2 = "2 3 4 5 6 106 107  108  109 110; 0 0 0 0 0 0 0 0 0 0";
+  double bandwidth = 10.0;
+  int seg_num;
+  mat vis_map, clustCent;
+  CHECK( true == 
+          groupByFlows(xs1, xs2, bandwidth, seg_num, vis_map, clustCent));
+
+  // Check return values :
+  CHECK( 2 == seg_num);
+
+  mat expected_vis_map = "1 1 1 1 1 0 0 0 0 0; 0 0 0 0 0 1 1 1 1 1";
+  vis_map = trans(vis_map);
+  //TODO => CHECK( accu(vis_map == expected_vis_map) == expected_vis_map.n_elem );
+  
+  mat expected_clustCent = "8 0 -100 0; 3 0 -1 0"; // x, y, delta_x, delta_y
+  expected_clustCent = trans(expected_clustCent);
+  CHECK( accu(clustCent == expected_clustCent) == expected_clustCent.n_elem );
+}
+
+/* ************************************************************************* */
+int main() { TestResult tr; return TestRegistry::runAllTests(tr);}
+/* ************************************************************************* */
